8ef62fcdca885f1d3895c442df6b5125cc60fc30
homectl <homectl@homectl.org>
2021-04-18 21:29:32 +0000

    See https://github.com/homectl/workspace/commit/99f7d10da9ecf3a423c35d8002f40b0c14aee108

---
+++ src/Language/GLSL/Runtime/Eval.hs
@@ -0,0 +1,210 @@
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE RecordWildCards            #-}
+{-# LANGUAGE StrictData                 #-}
+{-# LANGUAGE ViewPatterns               #-}
+module Language.GLSL.Runtime.Eval where
+
+import           Control.Lens                     ((^.))
+import           Control.Monad                    (foldM, foldM_)
+import           Control.Monad.Trans.State.Strict (evalStateT, get, modify')
+import qualified Data.IntMap                      as M
+import qualified Data.Text.Lazy                   as LT
+import qualified Data.Text.Lazy.IO                as IO
+import qualified Debug.Trace                      as Trace
+import           Language.GLSL.Decls              (addDecl, addDeclN, addDeclNE,
+                                                   emptyDecls, getDeclNE,
+                                                   toUniformId)
+import qualified Language.GLSL.Runtime.PrimFuns   as PrimFuns
+import           Language.GLSL.Runtime.Value      (Eval, EvalResult (..),
+                                                   EvalState (..), Proc (..),
+                                                   Value (..), defaultValue,
+                                                   evalBinaryOp, evalCoerce,
+                                                   evalUnaryOp, isNaNValue,
+                                                   roundValue)
+import           Language.GLSL.Types
+import           Linear                           (R1 (..), R2 (..), R3 (..),
+                                                   R4 (..))
+
+
+traceAssignments :: Bool
+traceAssignments = False
+
+trace :: String -> a -> a
+trace = if traceAssignments then Trace.trace else const id
+
+
+eval :: LT.Text -> Either String Value
+eval code = do
+  glsl <- parseShader code
+  fromResult $ evalStateT (evalGLSL glsl) startState
+
+
+startState :: EvalState
+startState = EvalState
+  { stProcs = M.empty
+  , stMainProc = Nothing
+
+  , globals = emptyDecls
+  , gl_Position = Nothing
+  }
+
+
+evalGLSL :: GLSL () -> Eval Value
+evalGLSL (GLSL _ d) = do
+  mapM_ discoverTopDecl d
+  evalMain
+  -- maybe (FloatValue 0) id . gl_Position <$> get
+  roundValue <$> getValue emptyLocals (NameExpr (Name NsOut (NameId 0)))
+
+discoverTopDecl :: TopDecl () -> Eval ()
+discoverTopDecl (LayoutDecl _ d) = discoverGlobalDecl d
+discoverTopDecl (GlobalDecl d) = discoverGlobalDecl d
+discoverTopDecl (ProcDecl ProcMain params body) =
+  modify' $ \st -> st{stMainProc = Just $ Proc params body}
+discoverTopDecl (ProcDecl (ProcName (NameId n)) params body) =
+  modify' $ \st@EvalState{..} -> st{stProcs = M.insert n (Proc params body) stProcs}
+
+discoverGlobalDecl :: GlobalDecl -> Eval ()
+discoverGlobalDecl (GDecl GkUniform (TyStruct _ fields) (Name NsU n)) =
+  modify' $ \st@EvalState{..} ->
+    st{globals = foldr (\(ty, m) -> addDecl NsU (toUniformId (n, m)) (defaultValue ty)) globals fields}
+discoverGlobalDecl (GDecl GkOut ty n) =
+  modify' $ \st@EvalState{..} -> st{globals = addDeclN n (defaultValue ty) globals}
+discoverGlobalDecl (GDecl GkIn ty n) =
+  modify' $ \st@EvalState{..} -> st{globals = addDeclN n (defaultValue ty) globals}
+discoverGlobalDecl d@(GDecl GkUniform _ _) =
+  fail $ "unsupported uniform type in decl: " <> pp ppGlobalDecl d
+
+
+evalMain :: Eval ()
+evalMain = do
+  Just mainProc <- stMainProc <$> get
+  evalProc mainProc []
+  return ()
+
+newtype LocalState = LocalState
+  { temps :: M.IntMap Value
+  }
+
+emptyLocals :: LocalState
+emptyLocals = LocalState
+  { temps = M.empty
+  }
+
+evalProc :: Proc -> [Value] -> Eval ()
+evalProc (Proc _params ss) _args =
+  foldM_ evalStmtAnnot emptyLocals ss
+
+evalStmtAnnot :: LocalState -> StmtAnnot () -> Eval LocalState
+evalStmtAnnot lst (SA () s) = evalStmt lst s
+
+evalStmt :: LocalState -> Stmt () -> Eval LocalState
+evalStmt lst (AssignStmt n e) = do
+  v <- evalExpr lst e
+  setValue lst (NameExpr n) v
+evalStmt lst (DeclStmt d) = evalLocalDecl lst d
+evalStmt lst (EmitStmt e) = evalEmit lst e
+evalStmt lst (IfStmt cond thens elses) = do
+  BoolValue v <- getValue lst (NameExpr (Name NsT cond))
+  if v
+    then foldM evalStmtAnnot lst thens
+    else foldM evalStmtAnnot lst elses
+
+evalEmit :: LocalState -> Emit -> Eval LocalState
+evalEmit lst EmitFragDepth = return lst
+evalEmit lst (EmitPosition e) = do
+  v <- evalExpr lst e
+  modify' $ \st -> st{gl_Position = Just v}
+  return lst
+
+evalLocalDecl :: LocalState -> LocalDecl -> Eval LocalState
+evalLocalDecl lst (LDecl ty (NameExpr . Name NsT -> n) Nothing) =
+  let v = defaultValue ty in
+  setValue lst n v
+evalLocalDecl lst (LDecl ty (NameExpr . Name NsT -> n) (Just e)) = do
+  v <- evalExpr lst e >>= evalCoerce ty
+  setValue lst n v
+
+evalExpr :: LocalState -> Expr -> Eval Value
+evalExpr lst = \case
+  BinaryExpr l op r -> do
+    lv <- evalExprAtom lst l
+    rv <- evalExprAtom lst r
+    return $ evalBinaryOp lv op rv
+  UnaryExpr op e -> do
+    v <- evalExprAtom lst e
+    return $ evalUnaryOp op v
+  AtomExpr e -> evalExprAtom lst e
+  FunCallExpr fun args -> do
+    vals <- mapM (evalExprAtom lst) args
+    PrimFuns.eval fun vals
+  e@TextureExpr{} ->
+    fail $ "texture() not implemented: " <> pp ppExpr e
+
+evalExprAtom :: LocalState -> ExprAtom -> Eval Value
+evalExprAtom lst = \case
+  LitFloatExpr _ f   -> return $ FloatValue f
+  LitIntExpr _ i     -> return $ IntValue i
+  IdentifierExpr n   -> getValue lst n
+  SwizzleExpr n s    -> getValue lst (NameExpr (Name NsT n)) >>= evalVecIndex s
+  VecIndexExpr n i   -> getValue lst n >>= evalVecIndex i
+  MatIndexExpr n i j -> getValue lst n >>= evalMatIndex i j
+
+evalVecIndex :: Swizzle -> Value -> Eval Value
+evalVecIndex X (Vec2Value v) = return $ FloatValue $ v ^. _x
+evalVecIndex Y (Vec2Value v) = return $ FloatValue $ v ^. _y
+evalVecIndex X (Vec3Value v) = return $ FloatValue $ v ^. _x
+evalVecIndex Y (Vec3Value v) = return $ FloatValue $ v ^. _y
+evalVecIndex Z (Vec3Value v) = return $ FloatValue $ v ^. _z
+evalVecIndex X (Vec4Value v) = return $ FloatValue $ v ^. _x
+evalVecIndex Y (Vec4Value v) = return $ FloatValue $ v ^. _y
+evalVecIndex Z (Vec4Value v) = return $ FloatValue $ v ^. _z
+evalVecIndex W (Vec4Value v) = return $ FloatValue $ v ^. _w
+evalVecIndex s v = fail $ "cannot access " <> pp ppSwizzle s <> " on " <> show v
+
+evalMatIndex :: Swizzle -> Swizzle -> Value -> Eval Value
+evalMatIndex i j (Mat4x4Value v) =
+  return $ FloatValue $ v ^. (swizzle i . swizzle j)
+  where
+    swizzle X = _x
+    swizzle Y = _y
+    swizzle Z = _z
+    swizzle W = _w
+evalMatIndex i j v =
+  fail $ "cannot access [" <> pp ppVecIndex i <> "][" <> pp ppVecIndex j <> "]"
+      <> " on " <> show v
+
+
+setValue :: LocalState -> NameExpr -> Value -> Eval LocalState
+setValue lst@LocalState{..} n@(NameExpr (Name NsT (NameId nId))) v =
+  trace (pp ppNameExpr n <> " = " <> show v) $
+  if isNaNValue v
+    then fail $ pp ppNameExpr n <> " = " <> show v
+    else return lst{temps = M.insert nId v temps}
+setValue lst n v = do
+  modify' $ \st@EvalState{..} -> st{globals = addDeclNE n v globals}
+  trace (pp ppNameExpr n <> " = " <> show v) $
+    if isNaNValue v
+      then fail $ pp ppNameExpr n <> " = " <> show v
+      else return lst
+
+
+getValue :: LocalState -> NameExpr -> Eval Value
+getValue LocalState{..} (NameExpr (Name NsT (NameId n))) = do
+  let Just v = M.lookup n temps
+  return v
+getValue _ n = do
+  v <- getDeclNE n . globals <$> get
+  case v of
+    Nothing -> fail $ "undefined global: " <> pp ppNameExpr n
+    Just ok -> return ok
+
+
+main :: IO ()
+main = do
+  txt <- IO.readFile "../large-shaders/xax.vert"
+  print $ eval txt
---
+++ src/Language/GLSL/Runtime/PrimFuns.hs
@@ -0,0 +1,71 @@
+module Language.GLSL.Runtime.PrimFuns where
+
+import           Control.Monad               ((>=>))
+import           Language.GLSL.Runtime.Math  (floor, fract, mod, smoothstep,
+                                              step)
+import           Language.GLSL.Runtime.Value (Eval, Value (..), evalCoerce)
+import           Language.GLSL.Types         (FunName (..), Type (..), pp,
+                                              ppFunName)
+import           Linear
+import           Prelude                     hiding (floor, mod)
+
+flt :: Value -> Eval Float
+flt = evalCoerce TyFloat >=> convert
+  where
+    convert (FloatValue v) = pure v
+    convert v              = fail $ "not a Float value: " <> show v
+
+v4 :: Value -> Eval (V4 Float)
+v4 = evalCoerce (TyVec 4) >=> convert
+  where
+    convert (Vec4Value v) = pure v
+    convert v             = fail $ "not a V4 value: " <> show v
+
+
+eval :: FunName -> [Value] -> Eval Value
+eval PrimVec2 [x, y] =
+  fmap Vec2Value $ V2
+    <$> flt x
+    <*> flt y
+eval PrimVec3 [x, y, z] =
+  fmap Vec3Value $ V3
+    <$> flt x
+    <*> flt y
+    <*> flt z
+eval PrimVec4 [x, y, z, w] =
+  fmap Vec4Value $ V4
+    <$> flt x
+    <*> flt y
+    <*> flt z
+    <*> flt w
+
+eval PrimMat4x4 [x, y, z, w] =
+  fmap Mat4x4Value $ V4
+    <$> v4 x
+    <*> v4 y
+    <*> v4 z
+    <*> v4 w
+
+eval PrimLength [Vec2Value a] = pure $ FloatValue $ norm a
+eval PrimLength [Vec3Value a] = pure $ FloatValue $ norm a
+eval PrimLength [Vec4Value a] = pure $ FloatValue $ norm a
+
+eval PrimNormalize [Vec2Value a] = pure $ Vec2Value $ signorm a
+eval PrimNormalize [Vec3Value a] = pure $ Vec3Value $ signorm a
+eval PrimNormalize [Vec4Value a] = pure $ Vec4Value $ signorm a
+
+eval PrimSqrt   [a] = FloatValue      . sqrt   <$> flt a
+eval PrimSin    [a] = FloatValue      . sin    <$> flt a
+eval PrimAsin   [a] = FloatValue      . asin   <$> flt a
+eval PrimCos    [a] = FloatValue      . cos    <$> flt a
+eval PrimAbs    [a] = FloatValue      . abs    <$> flt a
+eval PrimFloor  [a] = FloatValue      . floor  <$> flt a
+eval PrimFract  [a] = FloatValue      . fract  <$> flt a
+eval PrimMod  [a,b] = fmap FloatValue $ mod    <$> flt a <*> flt b
+eval PrimAtan [a,b] = fmap FloatValue $ atan2  <$> flt a <*> flt b
+
+eval PrimSmoothstep [a,b,c] = fmap FloatValue $ smoothstep <$> flt a <*> flt b <*> flt c
+eval PrimStep         [a,b] = fmap FloatValue $ step <$> flt a <*> flt b
+
+-- eval _ (a:_) = pure a
+eval fun _ = fail $ "primfun not implemented: " <> pp ppFunName fun
